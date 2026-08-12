53babbd250cd5fb297fd6f6d83685e7abf32e823
kewada661 <kewada661@gmail.com>
2026-08-11 22:51:41 -0700

    Fixed throw statements

--- src/services/gradeService.ts
+++ src/services/gradeService.ts
@@ -6,7 +6,7 @@ export const getGrades = async (user_id: number, case_id: number) => {
 
 export const upsertGrades = async (user_id: number, case_id: number, grade: number) => {
   if (!user_id || !case_id || !grade) {
-    throw new Error("Missing user ID, case ID, or grade", 400);
+    throw new StatusError("Missing user ID, case ID, or grade", 400);
   }
 
   gradeRepo.upsertGrades(user_id, case_id, grade);
--- src/services/timesService.ts
+++ src/services/timesService.ts
@@ -3,7 +3,7 @@ import { timeRepo } from "../db/index.js";
 export const getTimes = async (user_id: number, case_id: number, option: string) => {
   let times;
   if (!user_id) {
-    throw new Error("Missing user ID", 400);
+    throw new StatusError("Missing user ID", 400);
   }
   if (case_id) {
     // Do something
@@ -13,34 +13,34 @@ export const getTimes = async (user_id: number, case_id: number, option: string)
   }
 
   if (!times) {
-    throw new Error("Times not found", 404);
+    throw new StatusError("Times not found", 404);
   }
   return times;
 }
 
 export const postTimes = async (user_id: number, case_id: number, ms_elapsed: number) => {
   if (!user_id || !case_id) {
-    throw new Error ("Missing user ID or case ID", 400)
+    throw new StatusError ("Missing user ID or case ID", 400)
   }
   if (Number.isNaN(ms_elapsed)) {
-    throw new Error ("ms_elapsed is NaN", 400);
+    throw new StatusError ("ms_elapsed is NaN", 400);
   }
   timeRepo.insertTimes(user_id, case_id, ms_elapsed);
 }
 
 export const patchTimes = async (time_id: number, ms_elapsed: number) => {
   if (!time_id) {
-    throw new Error ("Missing time ID, user ID, or case ID", 400);
+    throw new StatusError ("Missing time ID, user ID, or case ID", 400);
   }
   if (Number.isNaN(ms_elapsed)) {
-    throw new Error ("ms_elapsed is NaN", 400);
+    throw new StatusError ("ms_elapsed is NaN", 400);
   }
   timeRepo.updateTimes(time_id, ms_elapsed);
 }
 
 export const deleteTimes = async (time_id: number) => {
   if (!time_id) {
-    throw new Error ("Missing time ID", 400);
+    throw new StatusError ("Missing time ID", 400);
   }
   timeRepo.deleteTimes(time_id);
 }
